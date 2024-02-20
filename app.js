2c4e488234babc3469cb6b26b47cce79a8442f31
asif12018 <asif.request@gmail.com>
2024-02-20 15:12:52 +0600

    data base added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require('lodash');
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,13 +19,42 @@ app.use(express.static("public"));
 
 var posts = [];
 
+//1.create a data base
+mongoose.connect('mongodb://localhost:27017/blogDB');
+//2.create a schema
+const postsSchema ={
+  title:String,
+  post:String
+};
+//3.create new model
+const Post = mongoose.model('Post',
+postsSchema
+);
+//4.create sample data
+const post1 = new Post({
+  title:'Day 01',
+  post:'this is just a text post'
+});
+
+//inserting data to array
+posts.push(post1);
+//inserting to data base
+Post.insertMany(posts);
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
 
 
 app.get('/', function(req,res){
-  res.render('home',{content:homeStartingContent, posts:posts});
+   Post.find({})
+   .then((foundPosts)=>{
+    if(foundPosts.length === 0){
+      res.redirect('/');
+    }else{
+      res.render('home',{content:homeStartingContent, posts:foundPosts});
+    }
+   })
 });
 
 app.get('/about', function(req,res){
@@ -40,28 +70,48 @@ app.get('/compose',function(req,res){
 });
 
 app.post('/compose', function(req,res){
-  var post ={
+  const post = new Post({
     title: req.body.contentTitle,
-    content: req.body.content
-  }
-  posts.push(post);
+    post: req.body.content
+  });
+  post.save();
   res.redirect('/')
 });
 
 app.get('/post/:postName',function(req,res){
   var requestedTitle = _.lowerCase(req.params.postName);
-  console.log(req.params);
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-    
-    if(storedTitle === requestedTitle){
-      res.render('post',
+  // console.log(req.params);
+  Post.find({})
+  .then((foundPosts)=>{
+    for(let foundPost of foundPosts){
+      const storedTitle =  _.lowerCase(foundPost.title);
+      if(storedTitle === requestedTitle){
+        res.render('post',
       {
-        title: post.title,
-        content: post.content
+        title: foundPost.title,
+        post: foundPost.post,
+        id: foundPost._id
       });
+      }
     }
-
   })
   
+});
+
+//delete from database
+app.post('/delete',function(req,res){
+  const postId = req.body.submit;
+  Post.findByIdAndDelete(postId)
+  .then(deletedPost =>{
+    if(!deletedPost){
+      console.log('no porduct found with that ID');
+    }else{
+      console.log('Product deleted:', deletedPost)
+    }
+  })
+  .catch(err => {
+    console.error('Error deleting product:', err);
+});
+res.redirect('/');
 })
+
